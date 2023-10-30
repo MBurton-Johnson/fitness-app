5a6074cd199d2551ddae5f6f78deacebd34fff05
jeeivan <jeeivansri@gmail.com>
2023-10-30 16:32:41 +0000

    Added google login

---
+++ components/NavBar.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+import Link from "next/link"
+import { signIn, signOut, useSession } from "next-auth/react"
+
+export default function NavBar() {
+    const {status} = useSession()
+    return (
+        <div className="p-4 flex justify-between items-center">
+            <Link className="font-bold text-lg text-blue-700" href={"/"}>Home</Link>
+            {status === "authenticated" ? (
+                <button className="bg-slate-900 text-white px-6 py-2 rounded-md" onClick={() => signOut()}>Sign Out</button>
+            ) : (
+                <button className="bg-slate-900 text-white px-6 py-2 rounded-md" onClick={() => signIn('google')}>Sign In</button>
+            )}
+        </div>
+
+    )
+}
