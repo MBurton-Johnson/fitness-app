dccaa113bfd92d49e4720781900b7f899d71ebf4
AdrianaIaffa <adrianaiaffa@hotmail.com>
2023-11-01 14:31:13 +0000

    changes to addworkout

--- components/AddWorkout/AddWorkoutContext.js
+++ components/AddWorkout/AddWorkoutContext.js
@@ -1,20 +1,11 @@
 'use client';
-
-import { useParams } from "next/navigation";
-import { useSession } from "next-auth/react";
-
-
-export async function addWorkoutToDatabase(requestData) {
-const { id } = useParams()
+export async function addWorkoutToDatabase(requestData, id) {
 
     if (requestData.method === 'POST') {
       const { workoutName, exercises, cardio } = requestData.body;
-      // const id = req.params.id
-      
-
   
       // Send a POST request to create a new workout
-      const createWorkoutResponse = await fetch('http://localhost:3006/workouts/new', {
+      const createWorkoutResponse = await fetch(`http://localhost:3006/workouts/new/${id}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- components/WorkoutDetails/WorkoutDetailsApi.jsx
+++ components/WorkoutDetails/WorkoutDetailsApi.jsx
@@ -1,14 +1,4 @@
-import React from 'react';
 
-// export default function WorkoutDetailPage({ workout }) {
-//   return (
-//     <div>
-//       <h1>Workout Details</h1>
-//       <p>Workout Name: {workout.workoutName}</p>
-//       {/* Display other workout details */}
-//     </div>
-//   );
-// }
 export async function getServerSideProps(context) {
     const { id } = context.query;
     const res = await fetch(`http://localhost:3006/workouts/${id}`);
